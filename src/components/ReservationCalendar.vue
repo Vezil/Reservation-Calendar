4908f892065065116953b473095931e79b870954
Vezil <SzymonWojaczek@wp.pl>
2021-12-15 18:20:16 +0100

    add moment and range init

--- src/components/ReservationCalendar.vue
+++ src/components/ReservationCalendar.vue
@@ -3,6 +3,11 @@
 </template>
 
 <script>
+import Moment from 'moment';
+import { extendMoment } from 'moment-range';
+
+const moment = extendMoment(Moment);
+
 export default {
     name: 'ReservationCalendar',
     props: {
@@ -26,6 +31,16 @@ export default {
             type: Array,
             required: false,
             default: () => []
+        }
+    },
+    mounted() {
+        const start = moment('2021-01-01', 'YYYY-MM-DD');
+        const end = moment('2021-12-31', 'YYYY-MM-DD');
+
+        const range = moment.range(start, end);
+
+        for (const day of range.by('day')) {
+            console.log(day.format('YYYY-MM-DD'));
         }
     }
 };
